5337c098f58b9b47e219487e492d6501c912f984
aswin r <2227@tkmce.ac.in>
2023-07-05 12:41:55 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
 const app = express();
 
 const dbURI = `mongodb://localhost:27017`;
 
-const PORT = 3000;
+const PORT = 4000;
 
-mongoose.connect(dbURI).then( (result) => {
-    app.listen(PORT, () => { console.log(`listening on http://localhost:3000/`)});
-}).catch( (err) => console.log(err));
-
-
-
+mongoose
+  .connect(dbURI)
+  .then((result) => {
+    app.listen(PORT, () => {
+      console.log(`listening on http://localhost:4000/`);
+    });
+  })
+  .catch((err) => console.log(err));
 
 //setting up view engine
 app.set("view engine", "ejs");
@@ -22,9 +25,6 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
-app.get("/",(req,res) => {
-
-    res.render("index");
+app.get("/", (req, res) => {
+  res.render("index");
 });
